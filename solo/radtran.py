81b28671d023be1353f3074a03f18ef79d68f29f
Víctor Molina García <victor@goa.uva.es>
2018-01-16 17:19:11 +0100

    Ensure shapes in radtran solver
    After the removal of the squeeze option, a check on all output shapes
    is desirable. This commit updates the radtran solver.

--- solo/radtran.py
+++ solo/radtran.py
@@ -15,7 +15,7 @@ def radtran(geo, atm, wvln=None, coupling=True):
         atm : Atmosphere
             an instance of Atmosphere containing the relevant information
             of the atmospheric components
-        wvln : array-like, shape (nwvln?,), optional
+        wvln : array-like, shape (nwvln,), optional
             wavelengths in nanometers (default None, which means that
             the wavelengths are taken from the same file where the TOA
             irradiance is stored)
@@ -25,15 +25,12 @@ def radtran(geo, atm, wvln=None, coupling=True):
 
     Return:
 
-        irr_glb : array-like, shape (nscen?, ngeo?, nwvln?)
-            BOA global irradiance for every scenario, geometry and
-            wavelength
-        irr_dir : array-like, shape (nscen?, ngeo?, nwvln?)
-            BOA direct irradiance for every scenario, geometry and
-            wavelength
-        irr_dif : array-like, shape (nscen?, ngeo?, nwvln?)
-            BOA diffuse irradiance for every scenario, geometry and
-            wavelength
+        irr_glb : array-like, shape (nscen, nwvln)
+            BOA global irradiance for every scenario and wavelength
+        irr_dir : array-like, shape (nscen, nwvln)
+            BOA direct irradiance for every scenario and wavelength
+        irr_dif : array-like, shape (nscen, nwvln)
+            BOA diffuse irradiance for every scenario and wavelength
 
     Raise:
 
@@ -48,20 +45,18 @@ def radtran(geo, atm, wvln=None, coupling=True):
     wvln0, irr0 = np.loadtxt(path).T
 
     # Ensure consistency of the input arguments.
-    if wvln is None:
-        wvln = wvln0
-    if len(np.shape(wvln)) > 1:
+    wvln = np.atleast_1d(wvln0 if wvln is None else wvln)
+    if np.ndim(wvln) > 1:
         raise ValueError("'wvln' must be 0- or 1-dimensional")
 
     # Convert wavelengths from nanometers to microns and adjust the TOA
     # irradiance to the actual Sun-Earth distance.
     wvln_um = 1E-3 * wvln
-    irr0 = irr0 * np.atleast_1d(geo.geometric_factor())[:, None]
+    irr0 = irr0 * geo.geometric_factor()[:, None]
 
     # Compute the transmittance due to Rayleigh and aerosols.
     args = [wvln_um, geo.mu0, True, coupling]
     tglb_mix, tdir_mix, tdif_mix, atm_alb = atm.trn_mixture(*args)
-    atm_rho = np.atleast_1d(atm.rho)[:, None]
 
     # Compute the transmittance due to gas absorption.
     args = [wvln, geo.mu0]
@@ -71,10 +66,10 @@ def radtran(geo, atm, wvln=None, coupling=True):
     tdir_gas = tdir_wat * tdir_ozo * tdir_oxy
 
     # Compute the amplification factor for the BOA global irradiance.
-    amp_factor = 1. / (1. - atm_rho * atm_alb)
+    amp_factor = 1. / (1. - atm.rho[:, None] * atm_alb)
 
     # Compute the BOA global, direct and diffuse irradiances.
-    mu0 = np.atleast_1d(geo.mu0)[:, None]
+    mu0 = geo.mu0[:, None]
     irr_glb = irr0 * mu0 * tglb_mix * tdir_gas * amp_factor
     irr_dir = irr0 * tdir_mix * tdir_gas
     irr_dif = irr_glb - irr_dir * mu0
