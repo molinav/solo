7992b2df08c9b1eff0d952aa01bf33af652989f5
Víctor Molina García <victor@goa.uva.es>
2018-01-17 13:37:19 +0100

    Add test option to setup.py command
    This commit adds the possibility to run the unit tests through the
    'setup.py' program by typing:
    
        python3 setup.py test

--- setup.py
+++ setup.py
@@ -1,11 +1,39 @@
 #! /usr/bin/env python3
 
+from distutils.cmd import Command
 try:
     from setuptools import setup
     install_requires = {"install_requires": "numpy"}
 except ImportError:
     from distutils.core import setup 
     install_requires = {}
+
+
+class UnittestCommand(Command):
+    """Custom command to run the unit tests from the library."""
+
+    description = "run unit tests from the library"
+    user_options = []
+
+    def initialize_options(self):
+        import sys
+        import os.path
+
+        name = "__init__.py"
+        fold = os.path.join(os.path.dirname(__file__), "solo", "test")
+        path = os.path.join(fold, name)
+
+        version = "{}".format(3 if sys.hexversion >= 0x03000000 else "")
+        python_exec = "python{}".format(version)
+        self.cmdlist = [python_exec, path]
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        subprocess.check_call(self.cmdlist)
+
 
 kwargs = {
     "name":
@@ -39,7 +67,11 @@ kwargs = {
             "test/obj/geo/*.dat",
         ]
     },
+    "cmdclass": {
+        "test": UnittestCommand,
+    },
 }
+
 kwargs.update(install_requires)
 setup(**kwargs)
 
