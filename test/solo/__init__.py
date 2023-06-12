7e546ec068f98433f130bcb416a0a7d2ba02bb48
Víctor Molina García <victor@goa.uva.es>
2023-06-12 16:51:40 +0200

    Fix missing docstring in test.solo

--- test/solo/__init__.py
+++ test/solo/__init__.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with solo; if not, see <https://www.gnu.org/licenses/>.
 #
+"""Template class for :mod:`solo` test cases."""
 
 import os.path
 try:
