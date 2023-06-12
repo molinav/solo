6bd1ef20f1520e8adfb0d11964a8118ac32e9f0f
Víctor Molina García <victor@goa.uva.es>
2023-06-12 13:55:04 +0200

    Add GPL copyright headers to all source code files

--- solo/api/__init__.py
+++ solo/api/__init__.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017-2019, 2023 Víctor Molina García
+#
+# This file is part of solo.
+#
+# solo is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# solo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with solo; if not, see <https://www.gnu.org/licenses/>.
+#
+
 from . Atmosphere import Atmosphere
 from . Geometry import Geometry
 
