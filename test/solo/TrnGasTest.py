df4fe9d68dfcb12b3002b0999c4e1b66ad2870d1
Víctor Molina García <victor@goa.uva.es>
2023-06-12 15:18:55 +0200

    Remove unused variables in tests

--- test/solo/TrnGasTest.py
+++ test/solo/TrnGasTest.py
@@ -90,7 +90,6 @@ class TrnGasTest(SoloTest):
         obj0 = self.result["tdir_gas"]
         shp1 = shp0 + self.one()
         obj1 = self.calcObj1(self.geo1, self.atm1, self.wvln[0])
-        args = [self.wvln[0], self.geo1.mu0]
         flag = np.allclose(obj1[0, 0], obj0[0], self.delta)
         self.assertTupleEqual(obj1.shape, shp1)
         self.assertTrue(flag)
