56c6938bf63fd373d60485fe3a73f40ccc8bc91b
Víctor Molina García <victor@goa.uva.es>
2023-08-12 18:42:02 +0200

    Rewrite Geometry tests using assertEqual

--- test/solo/test_Geometry.py
+++ test/solo/test_Geometry.py
@@ -37,25 +37,6 @@ GEOMETRY_FOLDER = os.path.join(UNITTEST_FOLDER, "obj", "geo")
 class TestGeometry(TestSolo):
     """Basic tests for the :class:`Geometry` class."""
 
-    def check_geo_equal(self, geo1, geo2):
-        """Generic equal check for :class:`Geometry` classes."""
-
-        self.assertTrue(np.allclose(geo1.day, geo2.day))
-        if geo1.sec is None:
-            self.assertTrue(geo1.sec is geo2.sec)
-        else:
-            self.assertTrue(np.allclose(geo1.sec, geo2.sec))
-        if geo1.lat is None:
-            self.assertTrue(geo1.lat is geo2.lat)
-        else:
-            self.assertTrue(np.allclose(geo1.lat, geo2.lat))
-        if geo1.lon is None:
-            self.assertTrue(geo1.lon is geo2.lon)
-        else:
-            self.assertTrue(np.allclose(geo1.lon, geo2.lon))
-        self.assertTrue(np.allclose(geo1.sza, geo2.sza))
-        self.assertTrue(np.allclose(geo1.mu0, geo2.mu0))
-
     def test_geo11(self):
         """Test loading of `geo11.dat` from file."""
 
@@ -63,7 +44,7 @@ class TestGeometry(TestSolo):
         geo1 = Geometry.from_file(path)
         geo2 = Geometry(
             day=152, sec=None, lat=None, lon=None, sza=60, mode="deg")
-        self.check_geo_equal(geo1, geo2)
+        self.assertEqual(geo1, geo2)
 
     def test_geo12(self):
         """Test loading of `geo12.dat` from file."""
@@ -73,7 +54,7 @@ class TestGeometry(TestSolo):
         geo2 = Geometry(
             day=152, sec=25311, lat=0.49410271, lon=-0.28797933,
             sza=1.39777933, mode="rad")
-        self.check_geo_equal(geo1, geo2)
+        self.assertEqual(geo1, geo2)
 
     def test_geo13(self):
         """Test loading of `geo13.dat` from file."""
@@ -83,7 +64,7 @@ class TestGeometry(TestSolo):
         geo2 = Geometry(
             day=152, sec=43510, lat=0.49410271, lon=-0.28797933,
             sza=0.2546518, mode="rad")
-        self.check_geo_equal(geo1, geo2)
+        self.assertEqual(geo1, geo2)
 
     def test_geo21(self):
         """Test loading of `geo21.dat` from file."""
@@ -93,7 +74,7 @@ class TestGeometry(TestSolo):
         geo2 = Geometry(
             day=np.array([152, 152, 152, 152, 153]), sec=None, lat=None,
             lon=None, sza=np.array([60, 50.4, 15.1, 21, 75.]), mode="deg")
-        self.check_geo_equal(geo1, geo2)
+        self.assertEqual(geo1, geo2)
 
     def test_geo22(self):
         """Test loading of `geo22.dat` from file."""
@@ -107,7 +88,7 @@ class TestGeometry(TestSolo):
             lon=np.array([-0.28797933, 1.31772359, 0.6981317]),
             sza=np.array([1.39777933, 1.17809272, 0.98533964]),
             mode="rad")
-        self.check_geo_equal(geo1, geo2)
+        self.assertEqual(geo1, geo2)
 
     def test_geo23(self):
         """Test loading of `geo23.dat` from file."""
@@ -121,7 +102,7 @@ class TestGeometry(TestSolo):
             lon=np.array([-0.28797933, 1.31772359, 0.6981317]),
             sza=np.array([0.2546518, 1.28671359, 1.24504354]),
             mode="rad")
-        self.check_geo_equal(geo1, geo2)
+        self.assertEqual(geo1, geo2)
 
 
 if __name__ == "__main__":
